d7b7a2972a7bfbb573d0003dd5bcb5aa6f34599f
Gabriel Bowen <gabe.bowen@utah.edu>
2023-02-09 18:00:40 -0700

    Add prez script

--- presentation_script.R
+++ presentation_script.R
@@ -6,12 +6,32 @@ library(sp)
 
 # Get and prep data ----
 
+twiso = getIsoscapes("USTap")
+plot(twiso)
 
+twiso = stack(twiso$d18o, twiso$d18o_sd)
+plot(twiso)
+
+kod = subOrigData("d18O", group = "Modern human", mask = states)
 
 # Create hair isoscape and posterior probability maps ----
 
+hairiso = calRaster(kod, twiso)
 
+ssdata = data.frame("ID" = c("Region_1", "Region_2", "Region_3", "Region_4"), 
+                    "d18O" = c(9.9, 8.4, 9.2, 11))
+sspost = pdRaster(hairiso, ssdata)
 
 # Post-hoc analysis ----
 
+ca_ut = states[states$STATE_NAME %in% c("California", "Utah"),]
+ca_ut$STATE_NAME
+oddsRatio(sspost, ca_ut)
 
+ssup = unionP(sspost)
+
+qtlRaster(ssup, 0.1)
+
+slc = SpatialPoints(data.frame(rep(-111.9, 4), rep(40.7, 4)), CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+sswd = wDist(sspost, slc)
+plot(sswd)
